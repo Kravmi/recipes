dcdfd306ffd917b3d0b7d3a757327d95f5baed5e
Kravmi <144707588+Kravmi@users.noreply.github.com>
2025-02-03 17:56:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,11 @@ document.querySelector('.snacks').addEventListener('click', function(){
     location.assign('snacks.html')
 })
 
+let block = document.querySelector('.container')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 40 * dx / window.innerWidth / 2
+    let angleY = 40 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
